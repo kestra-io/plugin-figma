6aa780384db19827ee4ead8e452cf3cc462e1cb4
François Delbrayelle <fdelbrayelle@kestra.io>
2026-05-29 16:45:51 +0200

    chore: initial scaffold cleanup for plugin-figma

---
+++ src/main/java/io/kestra/plugin/figma/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Figma",
+    description = "Figma plugin for Kestra",
+    categories = PluginSubGroup.PluginCategory.DATA
+)
+package io.kestra.plugin.figma;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
